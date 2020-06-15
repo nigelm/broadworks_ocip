5248316bccaf28d671b50807888046b1f9f02a04
Nigel Metheringham <nigel.metheringham@redcentricplc.com>
2020-06-15 16:44:22 +0100

    Compare canonical XML rather than text

--- tests/test_basic_command_xml.py
+++ tests/test_basic_command_xml.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Tests for `broadworks_ocip` package."""
 import pytest  # noqa: F401
+from lxml import etree
 
 from broadworks_ocip import BroadworksAPI
 
@@ -12,17 +13,34 @@ BASIC_API_PARAMS = {
 }
 
 
-def command_xml(command, **kwargs):
+def canon_xml(inxml):
+    """
+    we XML canonicalise/pretty print this to prevent whitespace/quote ordering
+    issues and to make any diffs easier to read.
+    Returned XML is as a decoded string - prettier for diffs
+    """
+    return etree.tostring(etree.fromstring(inxml), pretty_print=True).decode()
+
+
+def compare_command_xml(wanted, command, **kwargs):
     """Create a Broadworks XML command framgment from the argumenta"""
     api = BroadworksAPI(**BASIC_API_PARAMS)
-    return api.get_command_xml(command, **kwargs)
+    generated = api.get_command_xml(command, **kwargs)
+    assert canon_xml(generated) == canon_xml(wanted)
 
 
 def test_command_xml():
-    assert command_xml("AuthenticationRequest", user_id="username@example.com") == (
-        b"<?xml version='1.0' encoding='ISO-8859-1'?>\n<BroadsoftDocument "
-        b'xmlns="C" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" protocol="OCI">'
-        b'<sessionId xmlns="">00000000-1111-2222-3333-444444444444</sessionId>'
-        b'<command xmlns="" xsi:type="AuthenticationRequest"><userId>username@example.com</userId>'
-        b"</command></BroadsoftDocument>"
+    compare_command_xml(
+        (
+            b'<?xml version="1.0" encoding="ISO-8859-1"?>\n'
+            b'<BroadsoftDocument protocol="OCI" xmlns="C" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+            b'<sessionId xmlns="">00000000-1111-2222-3333-444444444444</sessionId>'
+            b'<command xmlns="" xsi:type="AuthenticationRequest">'
+            b"<userId>username@example.com</userId></command></BroadsoftDocument>"
+        ),
+        "AuthenticationRequest",
+        user_id="username@example.com",
     )
+
+
+# end
