10aeeb3663a99f882b304d9c5494d1cc467fda2f
Nigel Metheringham <nigel.metheringham@gmail.com>
2022-01-17 21:22:48 +0000

    ci: updated range of versions we test against

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import tempfile
 import nox
 
 
-@nox.session(python=["3.8", "3.7", "3.9", "3.10"])
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest")
