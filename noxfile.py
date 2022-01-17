bff70520d8b03cdbf5b44b173320a943239287c7
Nigel Metheringham <nigel.metheringham@gmail.com>
2022-01-17 19:22:23 +0000

    test: added safety for version security checking

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,6 @@
 # noxfile.py
+import tempfile
+
 import nox
 
 
@@ -6,6 +8,22 @@ import nox
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest")
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 # end
