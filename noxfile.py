f16c27677f81a7d440f13103c1a68dc75a3e3263
Nigel Metheringham <nigel.metheringham@gmail.com>
2022-01-17 19:21:56 +0000

    test: added nox for multi-version testing

---
+++ noxfile.py
@@ -0,0 +1,11 @@
+# noxfile.py
+import nox
+
+
+@nox.session(python=["3.8", "3.7", "3.9", "3.10"])
+def tests(session):
+    session.run("poetry", "install", external=True)
+    session.run("pytest")
+
+
+# end
