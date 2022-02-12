21cbb24f22ae9c9111bebec117613d928727d24d
Nigel Metheringham <nigel.metheringham@gmail.com>
2022-02-12 21:22:01 +0000

    tests: updated nox configuration

--- noxfile.py
+++ noxfile.py
@@ -6,12 +6,14 @@ import nox
 
 @nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
 def tests(session):
+    session.install("poetry")
     session.run("poetry", "install", external=True)
     session.run("pytest")
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.10")
 def safety(session):
+    session.install("poetry")
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
