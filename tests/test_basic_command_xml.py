81ae17ebdf3a7d96a232afe01db4d2fe78d76462
Nigel Metheringham <nigel.metheringham@gmail.com>
2022-02-14 14:30:42 +0000

    fix: tweaked tests to avoid spurious mypy error

--- tests/test_basic_command_xml.py
+++ tests/test_basic_command_xml.py
@@ -7,14 +7,12 @@ from lxml import etree
 
 from broadworks_ocip import BroadworksAPI
 
-BASIC_API_PARAMS = {
-    "host": "localhost",
-    "username": "username@example.com",
-    "password": "password",
-    "session_id": "00000000-1111-2222-3333-444444444444",
-}
-
-api = BroadworksAPI(**BASIC_API_PARAMS)
+api = BroadworksAPI(
+    host="localhost",
+    username="username@example.com",
+    password="password",
+    session_id="00000000-1111-2222-3333-444444444444",
+)
 
 
 def canon_xml(inxml):
